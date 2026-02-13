90865ce649675a34596fbd3ae958d5639c1cc4b6
GabTM2 <gabrielinchimonte@outlook.com>
2026-02-13 01:40:29 -0600

    Delete Pagina para mi novia directory

--- Pagina para mi novia/xtra.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("openSurprise");
-  const content = document.getElementById("surpriseContent");
-
-  btn.addEventListener("click", () => {
-    content.classList.add("show");
-    btn.style.display = "none"; // Oculta el botón después de hacer clic
-  });
